7fc3b725563936e3f608f975a17893151de3a912
suneel <sunilsingh00can@gmail.com>
2024-10-30 17:24:02 -0600

    first change

---
+++ app/src/main/java/com/example/junit/MainActivity.java
@@ -0,0 +1,98 @@
+package com.example.junit;
+
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Initialize TextView, EditText, and Button
+        TextView tv = findViewById(R.id.textview);
+        EditText et = findViewById(R.id.editview);
+        Button btn = findViewById(R.id.btn);
+
+        // Set up onClickListener for button
+        btn.setOnClickListener(v -> {
+            String password = et.getText().toString();
+            if (checkPasswordComplexity(password)) {
+                tv.setText("Your password meets the requirements.");
+            } else {
+                tv.setText("You shall not pass!");
+            }
+        });
+    }
+
+    /**
+     * Checks if the password meets the complexity requirements.
+     *
+     * @param pw the password string to check
+     * @return true if the password meets all complexity requirements, false otherwise
+     */
+    boolean checkPasswordComplexity(String pw) {
+        boolean foundUpperCase = false;
+        boolean foundLowerCase = false;
+        boolean foundNumber = false;
+        boolean foundSpecial = false;
+
+        for (int i = 0; i < pw.length(); i++) {
+            char c = pw.charAt(i);
+
+            if (Character.isUpperCase(c)) {
+                foundUpperCase = true;
+            } else if (Character.isLowerCase(c)) {
+                foundLowerCase = true;
+            } else if (Character.isDigit(c)) {
+                foundNumber = true;
+            } else if (isSpecialCharacter(c)) {
+                foundSpecial = true;
+            }
+        }
+
+        if (!foundUpperCase) {
+            Toast.makeText(this, "Your password does not have an uppercase letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundLowerCase) {
+            Toast.makeText(this, "Your password does not have a lowercase letter", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundNumber) {
+            Toast.makeText(this, "Your password does not have a number", Toast.LENGTH_SHORT).show();
+            return false;
+        } else if (!foundSpecial) {
+            Toast.makeText(this, "Your password does not have a special character", Toast.LENGTH_SHORT).show();
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    /**
+     * Checks if the character is one of the specified special characters.
+     *
+     * @param c the character to check
+     * @return true if the character is a special character, false otherwise
+     */
+    boolean isSpecialCharacter(char c) {
+        switch (c) {
+            case '#':
+            case '$':
+            case '%':
+            case '^':
+            case '&':
+            case '*':
+            case '!':
+            case '@':
+            case '?':
+                return true;
+            default:
+                return false;
+        }
+    }
+}
